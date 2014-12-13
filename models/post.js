da5aea9c39947c2b0809a411a7e5cc7267dc83c6
Delmer Reed <delmer814@yahoo.com>
2014-12-13 13:41:49 -0800

    add basic author and post model

---
+++ models/post.js
@@ -0,0 +1,16 @@
+"use strict";
+
+module.exports = function(sequelize, DataTypes) {
+  var post = sequelize.define("post", {
+    title: DataTypes.STRING,
+    content: DataTypes.TEXT
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+
+  return post;
+};
