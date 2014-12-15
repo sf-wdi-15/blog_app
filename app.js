39a75c71802f34c80bb3312f82002d29b23c0413
Delmer Reed <delmer814@yahoo.com>
2014-12-15 09:18:40 -0800

    blog setup

--- app.js
+++ app.js
@@ -11,4 +11,62 @@ app.get("/", function (req, res) {
   res.render("site/home");
 });
 
+app.get("/authors", function (req, res) {
+  db.author.findAll().then(function (authors) {
+    res.render("authors/index", {authors: authors});
+  });
+});
+
+app.get("/authors/:id", function (req, res) {
+  db.author.find({
+    where: {
+      id: req.params.id
+    }, 
+    include: [db.post]
+  }).then(function (author) {
+    res.render("authors/show", {author: author})
+  });
+});
+
+app.get("/authors/:author_id/posts/new", function (req, res) {
+  db.author.find(req.params.author_id)
+    .then(function (author) {
+      res.render("posts/new", {author: author});
+    },
+    function () {
+      res.redirect("/authors");
+    });
+});
+
+app.post("/authors/:author_id/posts", function (req, res) {
+  var authorId = req.params.author_id;
+  db.author.find(authorId)
+    .then(function (author) {
+      db.post.create({
+        title: req.body.post.title,
+        content: req.body.post.content,
+        authorId: authorId
+      })
+        .then(function (post) {
+          res.redirect("/authors/" + authorId + "/posts/" + post.id );
+        });
+    });
+});
+
+app.get("/authors/:author_id/posts/:id", function (req, res) {
+  var authorId = req.params.author_id;
+  var postId = req.params.id;
+  db.post.find({
+    where: {
+      authorId: authorId,
+      id: postId
+    },
+    include: [db.author]
+  }).then(function (post) {
+      res.render("posts/show", {post: post});
+  })
+});
+
+
 app.listen(3000)
+
